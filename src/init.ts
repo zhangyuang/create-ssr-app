11595ea8a9155c6b444a7d21082233baea8ccfd8
zhangyuang <leonin0829@gmail.com>
2021-01-14 16:30:20 +0800

    chore: add log red

--- src/init.ts
+++ src/init.ts
@@ -7,6 +7,9 @@ import { dclone } from 'dclone'
 const logGreen = (text: string) => {
   console.log(`\x1B[32m ${text}`)
 }
+const logRed = (text: string) => {
+  console.log(`\x1B[31m ${text}`)
+}
 interface TemplateMap {
   [key: string]: string | undefined
 }
@@ -27,13 +30,13 @@ const init = async (options?: Options) => {
     'ssr-with-dva': 'https://github.com/ykfe/egg-react-ssr/tree/dev/example/ssr-with-dva'
   }
   if (!argv._[0]) {
-    logGreen('未指定项目名称 请使用格式 npm init ssr-app <project-name>')
+    logRed('未指定项目名称 请使用格式 npm init ssr-app <project-name>')
     return
   }
   const targetDir = argv._[0]
 
   if (fs.existsSync(targetDir)) {
-    logGreen(`${targetDir}文件夹已存在，请先删除`)
+    logRed(`${targetDir}文件夹已存在，请先删除`)
     return
   }
   if (templateMap[argv.template] === undefined) {
