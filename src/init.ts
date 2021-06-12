8fdcdb6494a4fa8c4baf992cbe23da6785e5cf14
zhangyuang <leonin0829@gmail.com>
2021-06-12 22:04:08 +0800

    fix: update template

--- src/init.ts
+++ src/init.ts
@@ -26,7 +26,6 @@ const init = async (options?: Options) => {
   const cwd = process.cwd()
 
   const templateMap: TemplateMap = {
-    spa: 'https://github.com/ykfe/ssr/tree/dev/example/midway-react-ssr',
     'serverless-react-ssr': 'https://github.com/ykfe/ssr/tree/dev/example/midway-react-ssr',
     'serverless-vue-ssr': 'https://github.com/ykfe/ssr/tree/dev/example/midway-vue-ssr',
     'midway-react-ssr': 'https://github.com/ykfe/ssr/tree/dev/example/midway-react-ssr',
